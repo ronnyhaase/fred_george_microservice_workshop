085e48e6625cd5c501817d7acaf64643756505d2
Ronny Haase <ronny.haase@dawandamail.com>
2017-06-23 10:43:00 +0200

    Mob: Day 1 progress

--- ruby_v3_beta/lib/rental_offer.rb
+++ ruby_v3_beta/lib/rental_offer.rb
@@ -11,45 +11,52 @@
 require 'securerandom'
 require 'rapids_rivers'
 
-# Understands the complete stream of messages on an event bus
-class RentalNeed
+# Publishes rental offers on an event bus on need
+class RentalOffer
   attr_reader :service_name
 
-  NEED = 'car_rental_offer'
+  OFFER = 'car_rental_offer'
 
-def initialize(host_ip, port)
-    @rapids_connection = RapidsRivers::RabbitMqRapids.new(host_ip, port)
-    @river = RapidsRivers::RabbitMqRiver.new(@rapids_connection)
+  def initialize(host_ip, port)
+    rapids_connection = RapidsRivers::RabbitMqRapids.new(host_ip, port)
+    @river = RapidsRivers::RabbitMqRiver.new(rapids_connection)
     # See river_test.rb for various functions River supports to aid in filtering, like:
     # @river.require_values("key", "value");  // Reject packet unless it has key:value pair
     # @river.require("key1", "key2");   # Reject packet unless it has key1 and key2
     # @river.forbid("key1", "key2");    # Reject packet if it does have key1 or key2
     # @river.interested_in("key1", "key2");  // Allows key1 and key2 to be queried and set in a packet
     # For any keys required, forbidden, or deemed interesting, accessor methods are created in Packet
-    @river.require_values(need: NEED)
-    @river.interested_in('solution')
-    @service_name = 'rental_need_ruby_' + SecureRandom.uuid
+    @river.require_values(need: OFFER)
+    @river.forbid('solution')
+    @service_name = 'supply_rental_offer_ruby_' + SecureRandom.uuid
   end
 
   def start
-    loop do
-      @rapids_connection.publish need_packet
-      puts " [<] Published a rental offer need on the bus:\n\t     #{need_packet.to_json}"
-      sleep 5
-    end
+    puts " [*] #{@service_name} waiting for traffic on RabbitMQ event bus ... To exit press CTRL+C"
+    @river.register(self)
   end
 
   def packet rapids_connection, packet, warnings
-    puts "Got an offer for a need!"
+    puts " [<] Need of offer received"
+    packet.solution = {
+      text: 'discount',
+      value: (1000 * rand()).to_i,
+      likelihood: rand().round(2)
+    }
+    rapids_connection.publish packet
+    puts " [>] Published offer:\n\t    #{packet.to_json}"
+  end
+
+  def on_error rapids_connection, errors
+    #puts " [x] #{errors}"
   end
 
   private
 
-    def need_packet
-      fields = { need: NEED, id: SecureRandom.uuid, user_id: [1, 2, 3, nil].sample }
+    def offer_packet
+      fields = { offer: OFFER }
       RapidsRivers::Packet.new fields
     end
-
 end
 
-RentalNeed.new(ARGV.shift, ARGV.shift).start
+RentalOffer.new(ARGV.shift, ARGV.shift).start
---
+++ ruby_v3_beta/lib/rental_solution_collector.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Copyright (c) 2017 by Fred George.
+# May be used freely except for training; license required for training.
+
+# For debugging...
+# require 'pry'
+# require 'pry-nav'
+
+require 'securerandom'
+require 'rapids_rivers'
+
+# Publishes rental offers on an event bus on need
+class RentalSolutionCollector
+  attr_reader :service_name
+
+  OFFER = 'car_rental_offer'
+
+  def initialize(host_ip, port)
+    rapids_connection = RapidsRivers::RabbitMqRapids.new(host_ip, port)
+    @river = RapidsRivers::RabbitMqRiver.new(rapids_connection)
+    @river.require('id', 'solution')
+    @solutions = {}
+    # See river_test.rb for various functions River supports to aid in filtering, like:
+    # @river.require_values("key", "value");  // Reject packet unless it has key:value pair
+    # @river.require("key1", "key2");   # Reject packet unless it has key1 and key2
+    # @river.forbid("key1", "key2");    # Reject packet if it does have key1 or key2
+    # @river.interested_in("key1", "key2");  // Allows key1 and key2 to be queried and set in a packet
+    # For any keys required, forbidden, or deemed interesting, accessor methods are created in Packet
+    @service_name = 'supply_rental_solution_collector_ruby_' + SecureRandom.uuid
+  end
+
+  def start
+    puts " [*] #{@service_name} waiting for traffic on RabbitMQ event bus ... To exit press CTRL+C"
+    @river.register(self)
+  end
+
+  def packet rapids_connection, packet, warnings
+    #puts packet.inspect
+    @solutions[packet.id] = better(@solutions[packet.id], packet)
+    puts "The winner is: #{@solutions[packet.id]}"
+  end
+
+  private
+
+    def better(champion, challenger)
+      return challenger unless champion
+      @solutions[challenger.id] = survivor(challenger, champion)
+    end
+
+    def survivor(challenger, champion)
+      power(challenger) > power(champion) ? challenger : champion
+    end
+
+    def power(candidate)
+      candidate.solution['likelihood'] * candidate.solution['value']
+    end
+end
+
+RentalSolutionCollector.new(ARGV.shift, ARGV.shift).start
